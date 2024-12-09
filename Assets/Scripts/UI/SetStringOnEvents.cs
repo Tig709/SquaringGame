a6310de9519cb73dda307273805f3d6f6c794be2
Tigo <tigo.stam@hva.nl>
2024-12-09 14:43:42 +0900

    What to do text now appears 4 seconds after clicking HL/IO button
    -Changed SetSTringOnEvents script to make it possible to have a delay

--- Assets/Scripts/UI/SetStringOnEvents.cs
+++ Assets/Scripts/UI/SetStringOnEvents.cs
@@ -1,16 +1,21 @@
 using Events.GameEvents;
+using System.Collections;
 using System.Collections.Generic;
 using TMPro;
 using UnityEngine;
 
 namespace UI
 {
+    /// <summary>
+    /// Sets text on one of the events. You can add a delay in editor
+    /// </summary>
     public class SetStringOnEvents : MonoBehaviour
     {
 
         [SerializeField] private List<GameEvent> eventsToSetStringOn = new List<GameEvent>();
         [SerializeField] private TextMeshProUGUI targetText;
         [SerializeField] private string newText;
+        [SerializeField] private float delay;
 
         private void Awake()
         {
@@ -20,6 +25,12 @@ namespace UI
 
         private void SetText()
         {
+            StartCoroutine(SetTextAfterSeconds());
+        }
+
+        private IEnumerator SetTextAfterSeconds()
+        {
+            yield return new WaitForSeconds(delay);
             targetText.text = newText;
         }
     }
